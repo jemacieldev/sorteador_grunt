4ee36aad9639199b4d8560f5815801422b6cc992
Je <jemacieldev@gmail.com>
2024-06-13 14:12:46 -0300

    add-

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,3 +103,4 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+};
