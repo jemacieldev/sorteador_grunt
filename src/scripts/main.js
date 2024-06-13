aba18d1901fb565a22fe35bd3c56635e3823bfad
Je <jemacieldev@gmail.com>
2024-06-13 12:51:52 -0300

    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded',function() {
-    document.getElementById('form-sorteador').addEventListener('submit', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = perseInt(numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
     })
