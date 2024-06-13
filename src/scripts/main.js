3c99d43acccadf22205c2763a82f023c1ea76624
Je <jemacieldev@gmail.com>
2024-06-13 13:13:35 -0300

    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,5 +8,6 @@ document.addEventListener('DOMContentLoaded',function() {
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
     })
 })
